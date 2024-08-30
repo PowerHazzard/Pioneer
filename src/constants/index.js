23375ae760a0d8f4752d42ba829ca2152c7371a2
Naasson hazzard <powerhazzard1027@gmail.com>
2024-08-30 05:11:22 -0700

    david-hansen

---
+++ src/constants/index.js
@@ -0,0 +1,580 @@
+import {
+  AI,
+  backend,
+  blockchain,
+  frontend,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  jobit,
+  tripguide,
+  threejs,
+  agile,
+  angular,
+  bootstrap,
+  c,
+  chatGPT,
+  django,
+  flask,
+  graphql,
+  illustrator,
+  java,
+  jest,
+  jira,
+  mui,
+  mysql,
+  vite,
+  nextjs,
+  nuxtjs,
+  photoshop,
+  php,
+  postgres,
+  postman,
+  python,
+  pytorch,
+  ruby,
+  SEO,
+  tensorflow,
+  vuejs,
+  webpack,
+  elasitsearch,
+  duckduckgo,
+  accubits,
+  zuhlke,
+  sass,
+  supabase,
+  chakuraUI,
+  christmas_decor,
+  reduxsaga,
+  mernEcommerce,
+  angularProSidebar,
+  materialKitReact,
+  vuePokemon,
+  chatGPTUI,
+  Go,
+  Rust,
+  ShellScripting,
+  Vyper,
+  cassandra,
+  IPFS,
+  web3,
+  Hasura,
+  Geth,
+  Hardhat,
+  Truffle,
+  Embark,
+  Ganache,
+  Remix,
+  Celo,
+  Grab,
+  inforive_Solutions,
+  InfoBeans,
+  Paxful,
+  Bitso,
+  Solidity,
+  Bitcovia,
+  ExamGenie,
+  Pinpoint,
+  Johnfowlerholiday,
+  Laravel,
+  github,
+  Firebase,
+  AWS,
+  Linux,
+  NextUI,
+  Jayway,
+  Codemill,
+  Zocks,
+  UpSorce,
+  Rackspace,
+  Mocha,
+  Reducto,
+  FastAPI,
+} from "../assets";
+
+export const name = "Pioneer Consulting";
+export const role =
+  "We are Developers, Designers, and Engineers for various industry.";
+
+export const overview =
+  " We are the innovative Software Developers, Designers, and Engineers with expertise in JavaScript Frameworks including react, vue, next.js, node.js, express.js and also python based Frameworks such as Django, Flask, FastAPI etc. We possess a strong knowledge of various frameworks and programming languages, as well as problem-solving skills that enable me to identify scalability issues and improve overall system processes. We can support many industries including Healthcare, AI, and Blockchain.";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "skills",
+    title: "Skills",
+  },
+  {
+    id: "project",
+    title: "Project",
+  },
+];
+
+const services = [
+  {
+    title: "Frontend Development",
+    icon: frontend,
+  },
+  {
+    title: "Backend Development",
+    icon: reactjs,
+  },
+  {
+    title: "UI/UX Design",
+    icon: blockchain,
+  },
+  {
+    title: "API Integration",
+    icon: AI,
+  },
+];
+
+const technologies = [
+  {
+    set_name: "Program Language",
+    skill_sets: [
+      {
+        name: "C/C++",
+        icon: c,
+      },
+      {
+        name: "Python",
+        icon: python,
+      },
+      {
+        name: "HTML5",
+        icon: html,
+      },
+      {
+        name: "CSS",
+        icon: css,
+      },
+      {
+        name: "JavaScript",
+        icon: javascript,
+      },
+      {
+        name: "TypeScript",
+        icon: typescript,
+      },
+      {
+        name: "PHP",
+        icon: php,
+      },
+      {
+        name: "Java",
+        icon: java,
+      },
+    ],
+  },
+  {
+    set_name: "Databases",
+    skill_sets: [
+      {
+        name: "MySQL",
+        icon: mysql,
+      },
+      {
+        name: "PostgreSQL",
+        icon: postgres,
+      },
+      {
+        name: "MongoDB",
+        icon: mongodb,
+      },
+      {
+        name: "GraphQL",
+        icon: graphql,
+      },
+      {
+        name: "Elasticsearch",
+        icon: elasitsearch,
+      },
+    ],
+  },
+  {
+    set_name: "Libraries & Frameworks",
+    skill_sets: [
+      {
+        name: "Node.js",
+        icon: nodejs,
+      },
+      {
+        name: "Angular",
+        icon: angular,
+      },
+      ,
+      {
+        name: "React",
+        icon: reactjs,
+      },
+      {
+        name: "Vue.js",
+        icon: vuejs,
+      },
+      {
+        name: "Redux",
+        icon: redux,
+      },
+
+      {
+        name: "Figma",
+        icon: figma,
+      },
+      {
+        name: "Laravel",
+        icon: Laravel,
+      },
+      {
+        name: "Next.js",
+        icon: nextjs,
+      },
+      {
+        name: "Material UI",
+        icon: mui,
+      },
+      {
+        name: "Chakura UI",
+        icon: chakuraUI,
+      },
+      {
+        name: "Jest",
+        icon: jest,
+      },
+      {
+        name: "Mocha",
+        icon: Mocha,
+      },
+      {
+        name: "Django",
+        icon: django,
+      },
+      {
+        name: "GraphQL",
+        icon: graphql,
+      },
+      {
+        name: "FastAPI",
+        icon: FastAPI,
+      },
+    ],
+  },
+  {
+    set_name: "Development Tools",
+    skill_sets: [
+      {
+        name: "Git",
+        icon: git,
+      },
+      {
+        name: "GitHub",
+        icon: github,
+      },
+      {
+        name: "Docker",
+        icon: docker,
+      },
+      {
+        name: "Jira",
+        icon: jira,
+      },
+      {
+        name: "Firebase",
+        icon: Firebase,
+      },
+      {
+        name: "Linux",
+        icon: Linux,
+      },
+    ],
+  },
+];
+
+const projects = [
+  {
+    name: "PAXFUL",
+    description:
+      "Paxful.com is a peer-to-peer (P2P) cryptocurrency marketplace that allows users to buy and sell bitcoin with each other using over 300 different payment methods.",
+    specials: [
+      {
+        title: "Smart Contract",
+        color: "bg-lime-600",
+      },
+    ],
+    tags: [
+      {
+        name: "react",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Webpack",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Bootstrap",
+        color: "text-red-500",
+      },
+    ],
+    image: Paxful,
+    // source_code_link: "https://www.moreton.com/",
+    demo_link: "https://paxful.com/",
+  },
+  {
+    name: "Bitso",
+    description:
+      "The platform provides users with access to a range of tools and resources, including real-time market data, advanced charting tools, and an API for developers. ",
+    specials: [
+      {
+        title: "Cryptocurrencies",
+        color: "bg-cyan-600",
+      },
+    ],
+    tags: [
+      {
+        name: "Next.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "react",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Node.js",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: Bitso,
+    // source_code_link: "https://www.moreton.com/",
+    demo_link: "https://bitso.com/",
+  },
+  {
+    name: "Bitcoiva",
+    description:
+      "Bitcoiva is the Next-generation crypto trading platform that supports various forms of transactions and direct purchase options. Instant, fast and easy transactions are possible with Bitcoiva. ",
+    specials: [
+      {
+        title: "BITCOVIA",
+        color: "bg-violet-600",
+      },
+    ],
+    tags: [
+      {
+        name: "react",
+        color: "green-text-gradient",
+      },
+    ],
+    image: Bitcovia,
+    // source_code_link: "https://www.moreton.com/",
+    demo_link: "https://bitcoiva.com/",
+  },
+  {
+    name: "ExamGenie",
+    description:
+      "Unique Computer Based Testing Tool with automatic scoring and performance breakdown",
+
+    tags: [
+      {
+        name: "Angular",
+        color: "text-red-600",
+      },
+      {
+        name: "TypeScript",
+        color: "text-indigo-400",
+      },
+      {
+        name: "Bootstrap",
+        color: "text-blue-600",
+      },
+    ],
+    image: ExamGenie,
+    // source_code_link: "https://www.moreton.com/",
+    demo_link: "https://examgenie.com/",
+  },
+  {
+    name: "Pinpoint",
+    description:
+      "Email Marketing solutions for the Window Covering Industry for Famility holiday canvas in UK.",
+
+    tags: [
+      {
+        name: "PHP",
+        color: "text-violet-500",
+      },
+      {
+        name: "Laravel",
+        color: "text-blue-400",
+      },
+    ],
+    image: Pinpoint,
+    // source_code_link: "https://www.moreton.com/",
+    demo_link: "https://pinpoint.com/",
+  },
+  {
+    name: "John Fowler",
+    description:
+      "Website for UK-based holiday park operator with over 60 years of experience in providing family-friendly holidays.",
+
+    tags: [
+      {
+        name: "React",
+        color: "text-blue-500",
+      },
+      {
+        name: "Vue.js",
+        color: "text-green-400",
+      },
+    ],
+    image: Johnfowlerholiday,
+    // source_code_link: "https://www.moreton.com/",
+    demo_link: "https://www.johnfowlerholidays.com/",
+  },
+  {
+    name: "Christmas decor",
+    description:
+      "This is an e-commerce website. The selected products are stored in the cart even after the page is reloaded thanks to local storage. There are various sliders written in vanilla js and slink js.",
+
+    tags: [
+      {
+        name: "html",
+        color: "text-blue-500",
+      },
+      {
+        name: "css",
+        color: "text-green-400",
+      },
+      {
+        name: "Javascript",
+        color: "text-violet-500",
+      },
+    ],
+    image: christmas_decor,
+    source_code_link: "https://github.com/PowerHazzard/christmas-decor",
+  },
+  {
+    name: "MERN Ecommerce",
+    description:
+      "An ecommerce store built with MERN stack, and utilizes third party API's. This ecommerce store enable three main different flows or implementations",
+    specials: [
+      {
+        title: "e-commerce",
+        color: "bg-violet-600",
+      },
+      {
+        title: "MERN",
+        color: "bg-green-600",
+      },
+    ],
+    tags: [
+      {
+        name: "react",
+        color: "text-blue-500",
+      },
+      {
+        name: "redux",
+        color: "text-green-400",
+      },
+      {
+        name: "Express.js",
+        color: "text-violet-500",
+      },
+      {
+        name: "Node.js",
+        color: "text-blue-500",
+      },
+      {
+        name: "Mongodb.js",
+        color: "text-red-500",
+      },
+    ],
+    image: mernEcommerce,
+    source_code_link: "https://github.com/PowerHazzard/mern-ecommerce",
+    demo_link: "https://mern-store.onrender.com/",
+  },
+  {
+    name: "Angular pro Sidebar",
+    description:
+      "Responsive sidebar template with dropdown menu built with angular 7 and bootstrap 4",
+    specials: [
+      {
+        title: "template",
+        color: "bg-blue-600",
+      },
+      {
+        title: "Sidebar",
+        color: "bg-red-600",
+      },
+    ],
+    tags: [
+      {
+        name: "Angular",
+        color: "text-blue-500",
+      },
+      {
+        name: "Bootstrap",
+        color: "text-green-400",
+      },
+      {
+        name: "TypeScript",
+        color: "text-violet-500",
+      },
+      {
+        name: "SaaS",
+        color: "text-blue-500",
+      },
+    ],
+    image: angularProSidebar,
+    source_code_link: "https://github.com/PowerHazzard/angular-pro-sidebar",
+    demo_link: "https://azouaoui-med.github.io/angular-pro-sidebar/demo/",
+  },
+  {
+    name: "ChatGPT UI",
+    description:
+      "Chatbot UI is an open source chat UI for AI models with ChatGPT.",
+    specials: [
+      {
+        title: "ChatGPT",
+        color: "bg-blue-600",
+      },
+    ],
+    tags: [
+      {
+        name: "ChatGPT",
+        color: "text-blue-500",
+      },
+      {
+        name: "Redux-saga",
+        color: "text-green-400",
+      },
+      {
+        name: "supabase",
+        color: "text-violet-500",
+      },
+      {
+        name: "API",
+        color: "text-blue-500",
+      },
+    ],
+    image: chatGPTUI,
+    source_code_link: "https://github.com/PowerHazzard/chatbot-ui",
+    demo_link: "https://chatbotui.com/",
+  },
+];
+
+export { services, technologies, projects };
